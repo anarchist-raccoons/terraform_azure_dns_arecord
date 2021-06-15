9c314df396b69b84d60bd14aa20f43ab06b329a6
Rory McNicholl <rory.mcnicholl@london.ac.uk>
2021-06-15 09:07:33 +0000

    Quoted references are deprecated

--- main.tf
+++ main.tf
@@ -1,8 +1,8 @@
 provider "azurerm" {
-  subscription_id = "${var.subscription_id}"
-  client_id = "${var.client_id}"
-  client_secret = "${var.client_secret}"
-  tenant_id = "${var.tenant_id}"
+  subscription_id = var.subscription_id
+  client_id = var.client_id
+  client_secret = var.client_secret
+  tenant_id = var.tenant_id
   features {}
 }
 
@@ -20,24 +20,24 @@ module "labels" {
 
   tags = { Name = module.labels.id }
 #  # Required
-#  environment = "${var.environment}"
-#  name = "${var.name}"
+#  environment = var.environment
+#  name = var.name
 #  # Optional
-#  namespace-org = "${var.namespace-org}"
-#  organization = "${var.org}"
+#  namespace-org = var.namespace-org
+#  organization = var.org
 #  delimiter = "-"
-#  owner = "${var.owner}"
-#  team = "${var.team}"
+#  owner = var.owner
+#  team = var.team
 #  tags {
-#    Name = "${module.labels.id}"
+#    Name = module.labels.id
 #  }
 }
 
 # Azure DNS A Record
 resource "azurerm_dns_a_record" "default" {
   name = "${module.labels.name}-${module.labels.environment}"
-  zone_name           = "${var.zone_name}"
-  resource_group_name = "${var.zone_resource_group}"
+  zone_name           = var.zone_name
+  resource_group_name = var.zone_resource_group
   ttl                 = 300
-  records             = ["${var.record}"]
+  records             = [var.record]
 }
