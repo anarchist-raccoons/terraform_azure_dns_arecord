cedb823a3471be6802bb3f4b71d1ee80ea339837
Rory McNicholl <rory.mcnicholl@london.ac.uk>
2023-04-03 13:36:18 +0100

    branch this next time :|

--- main.tf
+++ main.tf
@@ -1,37 +1,35 @@
 provider "azurerm" {
-  subscription_id = var.subscription_id
-  client_id = var.client_id
-  client_secret = var.client_secret
-  tenant_id = var.tenant_id
+  subscription_id = "${var.subscription_id}"
+  client_id = "${var.client_id}"
+  client_secret = "${var.client_secret}"
+  tenant_id = "${var.tenant_id}"
   features {}
 }
 
 # Labels
 module "labels" {
-   source = "github.com/netflash/terraform-local-label"
-#  source = "devops-workflow/label/local"
-#  version = "0.2.1"
+  source = "devops-workflow/label/local"
+  version = "0.2.1"
 
   # Required
-  environment = var.environment
-  name = var.name
+  environment = "${var.environment}"
+  name = "${var.name}"
   # Optional
-  namespace-org = var.namespace-org
-  organization = var.org
+  namespace-org = "${var.namespace-org}"
+  organization = "${var.org}"
   delimiter = "-"
-  owner = var.owner
-  team = var.team
+  owner = "${var.owner}"
+  team = "${var.team}"
   tags = {
-    Name = module.labels.id
+    Name = "${module.labels.id}"
   }
 }
 
 # Azure DNS A Record
 resource "azurerm_dns_a_record" "default" {
-#  name = "${module.labels.name}-${module.labels.environment}"
-  name = format("%s-%s", module.labels.name,module.labels.environment)
-  zone_name           = var.zone_name
-  resource_group_name = var.zone_resource_group
+  name = "${module.labels.name}-${module.labels.environment}"
+  zone_name           = "${var.zone_name}"
+  resource_group_name = "${var.zone_resource_group}"
   ttl                 = 300
-  records             = [var.record]
+  records             = ["${var.record}"]
 }
